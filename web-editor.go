3aebf118af6de091f1b318619966da982cd8f14e
Oleksandr Kryvonos <geraclion@gmail.com>
2025-05-16 23:28:19 +0200

    new version of editor

--- web-editor.go
+++ web-editor.go
@@ -15,7 +15,7 @@ type Page struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    body, _ := os.ReadFile("../web-editor.html")
+    body, _ := os.ReadFile("../web-editor-2025-05-16-23h13m.html")
     fmt.Fprintf(w,"%s", body)
 }
 
